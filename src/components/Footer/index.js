fc206dc83a453223b372257dac11a0f370b47203
hthomp15 <hailethomp@gmail.com>
2022-06-17 15:43:32 -0400

    initial page setup

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="mt-6">
+            <p className="text-center"> Made with <span role="img" aria-label="Heart">❤️</span> by Haile Thompson, May 2022</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Nav() { 
+  return (
+    <nav className="nav">
+     <h1>NHL Reference</h1>
+     <div>Back to Teams</div>
+    </nav>
+    );
+}
+
+export default Nav;
