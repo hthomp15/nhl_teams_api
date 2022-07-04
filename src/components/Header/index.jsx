c8f3c25e22e8559c19ed3953adc4cd67a7b2d655
hthomp15 <hailethomp@gmail.com>
2022-07-04 12:14:37 -0400

    update logo path

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Header() { 
   return (
     <nav className="w-full mb-4">
-     <h1 className="text-3xl font-mono font-bold text-center bg-blue-200/75 py-3 text-white"><Link to="/">NHL Reference</Link></h1>
+     <h1 className="text-3xl font-mono font-bold text-center bg-blue-200/75 py-3 text-white"><Link to="/nhl_teams_api">NHL Reference</Link></h1>
     </nav>
     );
 }
